c7004f35dcdcf6ff0e13c68349bcabd302874609
Juan Villamagua <juanvillamagua3@gmail.com>
2024-05-15 16:46:28 -0500

    Cambios clase05

---
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete03;
+
+import paquete01.Calificacion;
+import paquete01.Profesor;
+
+public class EjemploCuatro {
+    public static void main(String[] args) {
+        // crear un arreglo de objetos de tipo Calificacion
+        
+        Profesor profesor1 = new Profesor("Franco","nombraminento");
+        Profesor profesor2 = new Profesor("Richard","contrato");
+        
+        Calificacion [] calificaciones = new Calificacion[2];
+        
+        Calificacion c = new Calificacion(10, "Computación", profesor1);
+        Calificacion c2 = new Calificacion(9, "Electrónica" ,profesor2);
+        
+        calificaciones[0] = c;
+        calificaciones[1] = c2;
+        
+        for (int i = 0; i < calificaciones.length; i++) {
+            Calificacion objetoCalificacion = calificaciones[i];
+            System.out.printf("%s - %.2f - Profesor(%s)\n", 
+                    objetoCalificacion.obtenerNombreMateria(),
+                    objetoCalificacion.obtenerNota(),
+                    objetoCalificacion.obtenerProfesor().obtenerNombre());
+        }
+    }
+}
--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -9,7 +9,14 @@ public class Ejecutor2 {
         LibretaCalificacion libreta = new LibretaCalificacion(
                 nombreEstudiante,notas);
         libreta.establecerPromedio();
+        // Si yo comento esta linea ya no estaria llamado lo que tengo 
+        // en promedio y tomaria como valor lo que tiene como defecto que seria
+        // un double
         libreta.establecerPromedioCualitativo();
+        // Asi mismo si yo dejo comentada esta linea no me llama abasulutamente 
+        // nada y de igual forma toma como valor lo que este asiganado en el 
+        // promedio culitativo que seria un String y me presentaria null que es 
+        //lo que toma por defecto
         System.out.printf("%s\n ",libreta);
     }
 }
