efe68161b720ffb13a75105e97801b85d463fada
Juan Villamagua <juanvillamagua3@gmail.com>
2024-05-15 16:09:10 -0500

    Cambios clase05

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete01/LibretaCalificacion.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete01/LibretaCalificacion.java
@@ -98,7 +98,7 @@ public class LibretaCalificacion {
         //        cadena, obtenerPromedio(), obtenerPromedioCualitativo());
         
         cadena = String.format("%sPromedio calificaciones: %.2f\n"
-                 + "Promedio cuantitativo: %s\n",
+                 + "Promedio cuanlitativo: %s\n",
                 cadena, promedio, promedioCualitativo);
         return cadena;
     }
