9db0bd5008de594190c04eecf379fe52969025e2
Juan Villamagua <juanvillamagua3@gmail.com>
2024-05-16 15:50:32 -0500

    Cambios

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploTres.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploTres.java
@@ -11,8 +11,11 @@ public class EjemploTres {
     public static void main(String[] args) {
         // crear un arreglo de objetos de tipo Calificacion
         Calificacion [] calificaciones = {new Calificacion(10, "Computación"), 
+            // Es un objeto anonimo donde en cualquier momento se le va a asiganar 
+            // un valor
             new Calificacion(9, "Electrónica")};
-        
+            new Calificacion(9, "Electrónica")};
+    
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
             System.out.printf("%s - %.2f\n", 
